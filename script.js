ca5a925a431f62f4bda3992c2f58d9d70b87db0b
hafsaelkhattabi <hafssakhattabi35@gmail.com>
2025-02-14 12:48:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // API Key 
-const apiKey = 'apikey';
+const apiKey = '32e614c3d4bca9a71dd373dce1c1d439';
 
 // function to fetch weather data
-function getdata(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+
+// Event listener for the btn to show the weater data
+document.getElementById('btn').addEventListener('click', function() {
+
+    const city = document.getElementById('city').value;
+    console.log(city)
+    if (city) {
+        // show the weather with the getdata function
+        getdata(city);
+    } else {
+        // to enter the correct city name for get the weather
+        console.log('Please enter a city name');
+    }
+});
+function getdata(city) {
+   
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+    
     // Fetch weather data
     fetch(url)
         // the response as a json form
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             // the data pass with success 200 OK
             if (data.cod === 200) {
                 // the path of the data , temperature and description
@@ -20,7 +37,7 @@ function getdata(city) {
                 // Update the UI 
                 // we use the getelementbyid to update the content after click the button
                 document.getElementById('location').textContent = `Location: ${location}`;
-                document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
+               // document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
                 document.getElementById('description').textContent = `Condition: ${description}`;
                 // also update the style for show the weather with different style
                 document.getElementById('result').style.display = 'block';
@@ -31,16 +48,5 @@ function getdata(city) {
         })
         .catch(err => console.error("Error fetching data:", err)); // error handling when the data didn't fetch correctly
 }
-
-// Event listener for the btn to show the weater data
-document.getElementById('btn').addEventListener('click', function() {
-    const city = document.getElementById('city').value;
-
-    if (city) {
-        // show the weather with the getdata function
-        getdata(city);
-    } else {
-        // to enter the correct city name for get the weather
-        console.log('Please enter a city name');
-    }
-});
+            
+   
